7b1fce55dee740220b1fb4376beab9c11a9a059d
Patrick Minda <patrick.minda@hotmail.com>
2026-04-16 19:57:16 +1000

    Initialize guessing_game and variables projects with basic structure and dependencies

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,28 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+use colored::*;
+fn main() {
+    loop {  
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        let secret_number = rand::thread_rng().gen_range(1,101);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        println!("You guessed: {guess}");
+        println!("The secret number is: {secret_number}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!".green());
+                break;
+            }
+        }
+    }
+}
